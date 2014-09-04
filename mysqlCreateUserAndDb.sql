a7042767a92ebcea44172448213b6c35aec722c4
Artur Krivtsov <gwartur@gmail.com>
2014-09-04 18:29:27 +0400

    Create mysqlCreateUserAndDb.sql

---
+++ mysqlCreateUserAndDb.sql
@@ -0,0 +1,7 @@
+CREATE USER 'dbname'@'localhost' IDENTIFIED BY  'passwd';
+
+GRANT USAGE ON * . * TO  'dbname'@'localhost' IDENTIFIED BY  'passwd' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0 ;
+
+CREATE DATABASE IF NOT EXISTS  `dbname` ;
+
+GRANT ALL PRIVILEGES ON  `dbname` . * TO  'dbname'@'localhost';
